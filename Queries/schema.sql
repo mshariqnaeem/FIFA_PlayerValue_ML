ecd951ef9a4b271ad660259be1db43e1c62bd4cc
femiimam001 <olufemi.imam@gmail.com>
2022-03-28 23:49:44 -0400

    adding new changes to files

--- Queries/schema.sql
+++ Queries/schema.sql
@@ -1,7 +1,7 @@
 -- Creating tables for FIFASoccerDB
-CREATE TABLE Soccer_Players(
-    Play_Id int NOT NULL
-	Skill_Id int NOT NULL
+CREATE TABLE Soccer_Players (
+  Play_Id int NOT NULL
+  Skill_Id int NOT NULL
 	Players_Name varchar(25)
  	Age int
 	Height_Cm float
@@ -14,6 +14,7 @@ CREATE TABLE Soccer_Players(
 	International_Reputation_1_5 number
 	Skill_Moves_1_5  number
 	Work_Rate number
+  FOREIGN KEY (Skill_Id) REFERENCES Soccer_Skills (Skill_Id),
      PRIMARY KEY (Play_Id),
      UNIQUE (Player_name)
 );
