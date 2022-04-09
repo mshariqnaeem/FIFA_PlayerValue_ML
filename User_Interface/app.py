e2cae89ced0a9b1c414be24a37aeb36916dcf0b2
SydneyTK <sydney9797@gmail.com>
2022-04-09 18:12:56 -0400

    attempted Flask

--- User_Interface/app.py
+++ User_Interface/app.py
@@ -4,5 +4,7 @@ from flask import Flask, render_template, redirect, url_for
 app = Flask(__name__)
 
 @app.route("/")
-def index():
-    return render_template("index.html")
+def home():
+    return render_template('index.html')
+if __name__=='__main__':
+    app.run()
