df82efde650d3dbeda6888b05ae16e35706f0ec1
femiimam001 <olufemi.imam@gmail.com>
2022-04-14 00:31:21 -0400

    final changes

--- schema.sql
+++ schema.sql
@@ -3,13 +3,6 @@
 SELECT COUNT(id) 
 FROM	fifa_cleaned;
 
-- Player_Name count by Player_Id
-SELECT COUNT(fc.Player_Id), Player_Name
-FROM fifa_cleaned as fc
-LEFT JOIN modified_fifa_data as mf
-ON fc.Player_Id = mf.Player_Id
-GROUP BY mf.Player_Name
-ORDER BY mf.dept_no;
 
 
 ALTER TABLE fifa_cleaned RENAME id TO Player_Id;
